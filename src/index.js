30f50032b70d212c19c9636b12f7fa3cddf77d39
sarahannie <87903267+sarahannie@users.noreply.github.com>
2022-12-03 10:27:04 +0100

    home page helf way done

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// set the target element that will be collapsed or expanded (eg. navbar menu)
+const targetEl = document.getElementById('targetEl');
+
+// optionally set a trigger element (eg. a button, hamburger icon)
+const triggerEl = document.getElementById('triggerEl');
+
+// optional options with default values and callback functions
+const options = {
+  triggerEl: triggerEl,
+  onCollapse: () => {
+      console.log('element has been collapsed')
+  },
+  onExpand: () => {
+      console.log('element has been expanded')
+  },
+  onToggle: () => {
+      console.log('element has been toggled')
+  }
+};
+
+const collapse = new Collapse(targetEl, options);
+
+// show the target element
+collapse.expand();
+
+// hide the target element
+collapse.collapse();
+
+// toggle the visibility of the target element
+collapse.toggle();
+
